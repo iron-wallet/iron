280ce0c9bd9f42f4af0b17d029e2425966fe4e86
Miguel Palhas <mpalhas@gmail.com>
2024-03-15 16:31:23 +0000

    code review

--- gui/src/routes/_home.home/contracts.lazy.tsx
+++ gui/src/routes/_home.home/contracts.lazy.tsx
@@ -1,4 +1,5 @@
 import { zodResolver } from "@hookform/resolvers/zod";
+import { debounce } from "lodash-es";
 import {
   Button,
   Chip,
@@ -9,6 +10,7 @@ import {
 import { FieldValues, useForm } from "react-hook-form";
 import { z } from "zod";
 import { createLazyFileRoute } from "@tanstack/react-router";
+import { useState } from "react";
 
 import { Contract } from "@iron/types";
 import { useContracts, useNetworks } from "@/store";
@@ -28,14 +30,20 @@ export const Route = createLazyFileRoute("/_home/home/contracts")({
 export function Contracts() {
   const chainId = useNetworks((s) => s.current?.chain_id);
   const contracts = useContracts((s) => s.contracts);
+  const [filter, setFilter] = useState<string>("");
 
   return (
     <>
       <Navbar>Contracts</Navbar>
-      {chainId != 31337 && <AddressForm />}
-      {Array.from(contracts || []).map((contract) => (
-        <ContractView key={contract.address} contract={contract} />
-      ))}
+      {chainId != 31337 && <AddContractForm />}
+      <FilterForm onChange={(f) => setFilter(f.toLowerCase())} />
+      {Array.from(contracts || [])
+        .filter((contract) =>
+          `${contract.address} ${contract.name}`.toLowerCase().includes(filter),
+        )
+        .map((contract) => (
+          <ContractView key={contract.address} contract={contract} />
+        ))}
     </>
   );
 }
@@ -58,7 +66,7 @@ function ContractView({
   );
 }
 
-function AddressForm() {
+function AddContractForm() {
   const schema = z.object({
     address: z.string().regex(/^0x[a-fA-F0-9]{40}$/, "Invalid format"),
   });
@@ -78,9 +86,9 @@ function AddressForm() {
 
   return (
     <form onSubmit={handleSubmit(onSubmit)}>
-      <Stack direction="row" spacing={2}>
+      <Stack direction="row">
         <TextField
-          label="Contract Address"
+          label="Add by address"
           error={!!errors.address}
           helperText={errors.address?.message?.toString() || ""}
           fullWidth
@@ -93,6 +101,42 @@ function AddressForm() {
         >
           {isSubmitting ? <CircularProgress /> : "Add"}
         </Button>
+      </Stack>
+    </form>
+  );
+}
+
+function FilterForm({ onChange }: { onChange: (address: string) => void }) {
+  const schema = z.object({
+    query: z.string(),
+  });
+
+  const add = useContracts((s) => s.add);
+
+  const {
+    handleSubmit,
+    formState: { errors },
+    register,
+  } = useForm({
+    mode: "onChange",
+    resolver: zodResolver(schema),
+  });
+
+  const onSubmit = (data: FieldValues) => add(data.address);
+
+  return (
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <Stack direction="row" spacing={2}>
+        <TextField
+          label="Filter"
+          error={!!errors.query}
+          helperText={errors.query?.message?.toString() || ""}
+          fullWidth
+          {...register("query")}
+          onChange={debounce((e) => {
+            onChange(e.target.value);
+          })}
+        />
       </Stack>
     </form>
   );
